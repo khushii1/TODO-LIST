36a4015e42a9f91a56cf5b542738672418d0fb94
khushii1 <khushiagarwal270203@gmail.com>
2023-10-23 11:39:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const inputbox=document.getElementById("input-box");
+const listcontainer=document.getElementById("list-container");
+function Additem(){
+    if(inputbox.value === ''){
+        alert('you must write something');
+    }
+    else {
+        let li=document.createElement("li");
+        li.innerHTML=inputbox.value;
+        listcontainer.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputbox.value='';
+    savedata();
+}
+
+listcontainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        savedata();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        savedata();
+    }
+},false);
+function savedata(){
+    localStorage.setItem("data",listcontainer.innerHTML);
+}
+function showlist(){
+    listcontainer.innerHTML = localStorage.getItem("data");
+}
+showlist();
